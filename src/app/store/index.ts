773ffb06063760ce7491e951561dd84548593530
Amarjeet <amarjeetmistri41@gmail.com>
2026-04-20 18:55:19 +0530

    refactor(store): switch app imports to store barrel

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import productReducer from "../../features/product/slices/productSlice.ts"
+import cartReducer from "../../features/cart/slices/cartSlices.ts"
 
 export const store = configureStore({
     reducer: {
-        product: productReducer
+        product: productReducer,
+        cart : cartReducer
     }
 });
 export type RootState = ReturnType<typeof store.getState>
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import "./index.css";
 import AppRoutes from "./app/routes/AppRoutes";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./app/store/store";
+import { store } from "./app/store";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
